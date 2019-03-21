21ac9814b44ab0d0bec359fd6bcbf630bf0264d7
vushatov <48793266+vushatov@users.noreply.github.com>
2019-03-21 16:32:25 +0200

    Simulation

--- src/test/java/Onboarding.java
+++ src/test/java/Onboarding.java
@@ -5,55 +5,90 @@ import org.openqa.selenium.WebElement;
 public class Onboarding {
     static WebDriver driver = Driver.driver;
 
-    public void flowPortfolio (){
-        buyPortfolio();
+    public void flowPortfolio() {
+        // buyPortfolio();
         suitability();
     }
 
-    void buyPortfolio()
-    {
-        if (!driver.getCurrentUrl().equals("http://18.185.161.25/investment")){
+    void buyPortfolio() {
+        if (!driver.getCurrentUrl().equals("http://18.185.161.25/investment")) {
             driver.get("http://18.185.161.25/investment");
         }
         driver.findElement(By.xpath("//tr[1]//div[@class=\"buttons-container\"]//button")).click();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        if (!driver.getCurrentUrl().equals("http://18.185.161.25/client/onboardings")){
-            System.out.println("The onboardings page doesn't open after buying a portfolio, current URL"+ driver.getCurrentUrl());
+        wait(1000);
+        if (!driver.getCurrentUrl().equals("http://18.185.161.25/client/onboardings")) {
+            System.out.println("The onboardings page doesn't open after buying a portfolio, current URL" + driver.getCurrentUrl());
             driver.get("http://18.185.161.25/client/onboardings");
         }
     }
-    void suitability()
-    {
-        if (!driver.getCurrentUrl().equals("http://18.185.161.25/client/onboardings")){
+
+    void suitability() {
+        if (!driver.getCurrentUrl().equals("http://18.185.161.25/client/onboardings")) {
             driver.get("http://18.185.161.25/client/onboardings");
         }
         WebElement goToSuitability = driver.findElement(By.xpath("//div[@class=\"onboardings-list-container\"]//li"));
         goToSuitability.click();
+        wait(500);
+        if (driver.findElements(By.xpath("//p[1][@class=\"suitability-subtitle\"]")).size() != 0) {
+            driver.findElement(By.xpath("//p[@class=\"max\"]")).click(); //1st q slider
+            nextStep(); //next
+            driver.findElement(By.xpath("//div[@style=\"min-height: 0px; height: auto; transition-duration: 258ms;\"]/div/div/div/div")).click(); //open dropdown list of 2nd q
+            wait(500);
+            driver.findElement(By.xpath("//div[@role=\"document\"]//li[@data-value=\"Wealth preservation\"]")).click(); //select  item in the dropdown
+            wait(500);
+
+
+            nextStep(); //next to the 3rd q
+            wait(500);
+            driver.findElement(By.xpath("//table[1]//tr[1]/td[1]//input[@type=\"checkbox\"]")).click(); // select checkbox in the table
+            wait(500);
+
+            nextStep(); //go to the 4th step
+            driver.findElement(By.xpath("//table[1]//tr[9]//td[1]/div")).click(); //open the last dropdown list in the table
+            wait(500);
+            driver.findElement(By.xpath("//li[@data-value=\"3 years\"]/p[@class=\"menuBar\"]")).click(); //select itrem in the dropdown
+            wait(500);
+
+            nextStep(); //go to the 5th step
+            wait(500);
+            driver.findElement(By.xpath("//li[@class=\"rangeslider__label-item\" and @data-value=\"10000\"]")).click(); //set 5000 uevro
+            wait(500);
+            nextStep(); //go to the 6th step
+            wait(500);
+            driver.findElement(By.xpath("//input[@type=\"number\"]")).clear();
+            driver.findElement(By.xpath("//input[@type=\"number\"]")).sendKeys("12"); // enter months number
+            wait(500);
+            nextStep(); //go to the 7th step
+            wait(500);
+            driver.findElement(By.xpath("//div[@class=\"question\"]//button[1]/span[1]")).click(); //open the dropdownlist
+            wait(500);
+            driver.findElement(By.xpath("//ul[@role=\"menu\"]/li[5]")).click(); //select >500.000
+            wait(500);
+            nextStep(); //go to the 8th step
+            wait(500);
+            driver.findElement(By.xpath("//input[@id=\"number-input\"]")).sendKeys("100000"); //enter data euro
+            wait(500);
+            nextStep(); //go to the 9th step
+            wait(500);
+            nextStep(); //go to the next page
+
+
+        } else System.out.println("The Suitability page is not opened!");
+    }
+
+    void nextStep() {
+        driver.findElement(By.xpath("//div[@class=\"question__steps-buttons\"]/button[2]")).click();
+    }
+
+    void previousStep() {
+        driver.findElement(By.xpath("//div[@class=\"question__steps-buttons\"]/button[1]")).click();
+    }
+
+    void wait(int a) {
         try {
-            Thread.sleep(500);
+            Thread.sleep(a);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        if (driver.findElements(By.xpath("//p[1][@class=\"suitability-subtitle\"]")).size()!=0)
-        {
-            driver.findElement(By.xpath("//p[@class=\"max\"]")).click(); //1st q slider
-            driver.findElement(By.xpath("//div[@class=\"question__steps-buttons\"]//button[2]")).click(); //next
-            driver.findElement(By.xpath("//div[@style=\"min-height: 0px; height: auto; transition-duration: 258ms;\"]/div/div/div/div")).click(); //open dropdown list of 2nd q
-            driver.findElement(By.xpath("//div[@role=\"document\"]//li[@data-value=\"Wealth preservation\"]")).click(); //select  item in the dropdown
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            driver.findElement(By.xpath("//div[@icon=\"2\"]//button[1]")).click(); //press back
-            driver.findElement(By.xpath("//div[@class=\"question__steps-buttons\"]//button[2]")).click(); //press next
-            driver.findElement(By.xpath("//div[@icon=\"2\"]//button[2]")).click(); //next to the 3rd q
-
-        }else System.out.println("The Suitability page is not opened!");
     }
-
 }
