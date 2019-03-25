d5e2a33e5bc354135def03caf7957fe15c8a4453
vushatov <48793266+vushatov@users.noreply.github.com>
2019-03-25 16:42:55 +0200

    Simulation

--- src/test/java/Driver.java
+++ src/test/java/Driver.java
@@ -1,28 +1,23 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
-import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
     public static WebDriver driver = new ChromeDriver();
-    public static WebDriver driver1 = new FirefoxDriver();
+    //public static WebDriver driver = new FirefoxDriver();
+   // public static WebDriver driver = new OperaDriver();
+   // public static WebDriver driver = new EdgeDriver();
+
 
 
 
     Driver(WebDriver driver) {
-        System.out.println("Select browser: Chrome - 1, Firefix -2: ");
-        Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
-        switch (number){
-            case 1:
-                driver = new ChromeDriver();
-            case 2:
-                driver = new FirefoxDriver();
-        }
         if (driver == null)
             driver = new ChromeDriver();
 
--- src/test/java/MainClass.java
+++ src/test/java/MainClass.java
@@ -17,31 +17,36 @@ import java.util.concurrent.TimeUnit;
 
 public class MainClass {
 
-
     public static void main(String[] args) throws InterruptedException, IOException {
 
         System.setProperty("webdriver.gecko.driver", "C:/Users/user/IdeaProjects/qa/drivers/geckodriver.exe");
         System.setProperty("webdriver.chrome.driver", "C:/Users/user/IdeaProjects/qa/drivers/chromedriver.exe");
-         WebDriver driver = Driver.driver;
-
-         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-         driver.manage().window().maximize();
-         driver.get("http://18.185.161.25");
-         driver.findElement(By.xpath("//div[@class=\"decent-bar\"]//span[1]")).click();
+        System.setProperty("webdriver.edge.driver", "C:/Users/user/IdeaProjects/qa/drivers/MicrosoftWebDriver.exe");
+        System.setProperty("webdriver.opera.driver", "C:/Users/user/IdeaProjects/qa/drivers/operadriver.exe");
+       // System.setProperty("phantomjs.binary.path", "C:\\Users\\user\\IdeaProjects\\qa\\drivers\\phantomjs-2.0.0-windows\\bin\\phantomjs.exe");
+        WebDriver driver = Driver.driver;
 
 
-       Login testLogin = new Login();
-       testLogin.openBasePage();
-       testLogin.login();
-       System.out.println("Success test login page");
-       Onboarding onboarding = new Onboarding();
-       onboarding.flowPortfolio();
-       System.out.println("Success test onboarding");
-       DeleteAllPortfolios deleteAllPortfolios = new DeleteAllPortfolios();
-      // deleteAllPortfolios.del();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://18.185.161.25");
+        if (driver.findElements(By.xpath("//div[@class=\"decent-bar\"]//span[1]")).size()!=0){
+        driver.findElement(By.xpath("//div[@class=\"decent-bar\"]//span[1]")).click();}
+
+
+
+        Login testLogin = new Login();
+        testLogin.openBasePage();
+        testLogin.login();
+        System.out.println("Success test login page");
+        Onboarding onboarding = new Onboarding();
+        onboarding.flowPortfolio();
+        System.out.println("Success test onboarding");
+        DeleteAllPortfolios deleteAllPortfolios = new DeleteAllPortfolios();
+        // deleteAllPortfolios.del();
         System.out.println("Success delete all portfolios");
 
-       // driver.quit();
+        // driver.quit();
 
 
     }
